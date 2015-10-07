415fdc6cf415388d05929a44be019a0e96732af5
Anders Hoff <inconvergent@gmail.com>
2015-10-07 09:45:51 +0200

    longs

--- src/zonemap3d.pyx
+++ src/zonemap3d.pyx
@@ -9,7 +9,7 @@ from libc.stdlib cimport malloc, free, realloc
 from libc.math cimport sqrt
 
 
-from helpers cimport int_array_init
+from helpers cimport long_array_init
 from helpers cimport double_array_init
 
 import numpy as np
@@ -18,12 +18,12 @@ cimport numpy as np
 from time import time
 
 
-cdef int SIZE = 64
+cdef long SIZE = 64
 
 
 cdef class Zonemap3d:
 
-  def __init__(self, int nz):
+  def __init__(self, long nz):
     """
     """
 
@@ -41,11 +41,11 @@ cdef class Zonemap3d:
 
     return
 
-  def __cinit__(self, int nz, *arg, **args):
+  def __cinit__(self, long nz, *arg, **args):
 
-    cdef int total_zones = nz*nz*nz
+    cdef long total_zones = nz*nz*nz
 
-    self.VZ = <int *>malloc(SIZE*sizeof(int))
+    self.VZ = <long *>malloc(SIZE*sizeof(long))
 
     self.ZONES = <sZ **>malloc(total_zones*sizeof(sZ*))
 
@@ -53,7 +53,7 @@ cdef class Zonemap3d:
 
   def __dealloc__(self):
 
-    cdef int i
+    cdef long i
 
     for i in xrange(self.total_zones):
 
@@ -79,7 +79,7 @@ cdef class Zonemap3d:
   cdef void __init_zones(self) nogil:
     # somehow this did not work when executed inside cinit
 
-    cdef int i
+    cdef long i
     cdef sZ *zone
 
     for i in xrange(self.total_zones):
@@ -89,7 +89,7 @@ cdef class Zonemap3d:
       zone.i = i
       zone.size = SIZE
       zone.count = 0
-      zone.ZV = <int *>malloc(SIZE*sizeof(int))
+      zone.ZV = <long *>malloc(SIZE*sizeof(long))
 
       self.ZONES[i] = zone
 
@@ -98,26 +98,26 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __add_vertex(self, int v1) nogil:
+  cdef long __add_vertex(self, long v1) nogil:
     """
     """
 
-    cdef int vnum = self.vnum
+    cdef long vnum = self.vnum
 
     cdef double x = self.X[v1]
     cdef double y = self.Y[v1]
     cdef double z = self.Z[v1]
 
-    cdef int z1 = self.__get_z(x,y,z)
+    cdef long z1 = self.__get_z(x,y,z)
 
     self.__add_v_to_zone(z1, vnum)
     self.VZ[vnum] = z1
 
-    cdef int* new_vz
+    cdef long* new_vz
 
     if self.vnum>=self.vsize-1:
 
-      new_vz = <int *>realloc(self.VZ, self.vsize*2*sizeof(int))
+      new_vz = <long *>realloc(self.VZ, self.vsize*2*sizeof(long))
 
       if new_vz is not NULL:
         self.VZ = new_vz;
@@ -132,11 +132,11 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __del_vertex(self, int v1) nogil:
+  cdef long __del_vertex(self, long v1) nogil:
     """
     """
 
-    cdef int z1 = self.VZ[v1]
+    cdef long z1 = self.VZ[v1]
 
     self.__remove_v_from_zone(z1, v1)
     self.VZ[v1] = -1
@@ -146,7 +146,7 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __add_v_to_zone(self, int z1, int v1) nogil:
+  cdef long __add_v_to_zone(self, long z1, long v1) nogil:
 
     cdef sZ *zone = self.ZONES[z1]
 
@@ -161,10 +161,10 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __extend_zv_of_zone(self, sZ *zone) nogil:
+  cdef long __extend_zv_of_zone(self, sZ *zone) nogil:
 
-    cdef int new_size = zone.size*2
-    cdef int* new_zv = <int *>realloc(zone.ZV, new_size*sizeof(int))
+    cdef long new_size = zone.size*2
+    cdef long* new_zv = <long *>realloc(zone.ZV, new_size*sizeof(long))
 
     if new_zv is not NULL:
       zone.ZV = new_zv;
@@ -180,10 +180,10 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __remove_v_from_zone(self, int z1, int v1) nogil:
+  cdef long __remove_v_from_zone(self, long z1, long v1) nogil:
 
     cdef sZ *zone = self.ZONES[z1]
-    cdef int i
+    cdef long i
 
     for i in xrange(zone.count):
 
@@ -197,28 +197,28 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __get_z(self, double x, double y, double z) nogil:
+  cdef long __get_z(self, double x, double y, double z) nogil:
     """
     """
 
-    cdef int nz = self.nz
-    cdef int k = int(z*nz)
-    cdef int j = int(y*nz)
-    cdef int i = int(x*nz)
+    cdef long nz = self.nz
+    cdef long k = <long>(z*nz)
+    cdef long j = <long>(y*nz)
+    cdef long i = <long>(x*nz)
 
     return nz*nz*k + nz*j + i
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __update_v(self, int v1) nogil:
+  cdef long __update_v(self, long v1) nogil:
 
     cdef double x = self.X[v1]
     cdef double y = self.Y[v1]
     cdef double z = self.Z[v1]
-    cdef int new_zone = self.__get_z(x, y, z)
+    cdef long new_zone = self.__get_z(x, y, z)
 
-    cdef int old_zone = self.VZ[v1]
+    cdef long old_zone = self.VZ[v1]
 
     if old_zone<0:
       return -1
@@ -238,35 +238,35 @@ cdef class Zonemap3d:
   @cython.boundscheck(False)
   @cython.nonecheck(False)
   @cython.cdivision(True)
-  cdef int __sphere_is_free(self, double x, double y, double z, double rad) nogil:
+  cdef long __sphere_is_free(self, double x, double y, double z, double rad) nogil:
     """
-    tests if there is another vertex within rad of x,y. rad must be less than
+    tests if there is another vertex withlong* rad of x,y. rad must be less than
     the width of each zone.
     """
 
     #TODO: optimize this
 
-    cdef int i
-    cdef int j
+    cdef long i
+    cdef long j
     cdef sZ *zone
-    cdef int zi = self.__get_z(x,y,z)
+    cdef long zi = self.__get_z(x,y,z)
 
-    cdef int nz = self.nz
+    cdef long nz = self.nz
 
     cdef double dx
     cdef double dy
     cdef double dz
     cdef double rad2 = rad*rad
 
-    cdef int zz = int(z*nz)
-    cdef int zy = int(y*nz)
-    cdef int zx = int(x*nz)
+    cdef long zz = <long>(z*nz)
+    cdef long zy = <long>(y*nz)
+    cdef long zx = <long>(x*nz)
 
-    cdef int zvj
+    cdef long zvj
 
-    cdef int a
-    cdef int b
-    cdef int c
+    cdef long a
+    cdef long b
+    cdef long c
 
     for a in xrange(max(zx-1,0),min(zx+2,nz)):
       for b in xrange(max(zy-1,0),min(zy+2,nz)):
@@ -291,35 +291,35 @@ cdef class Zonemap3d:
   @cython.boundscheck(False)
   @cython.nonecheck(False)
   @cython.cdivision(True)
-  cdef int __sphere_is_free_ignore(self, double x, double y, double z, int v, double rad) nogil:
+  cdef long __sphere_is_free_ignore(self, double x, double y, double z, long v, double rad) nogil:
     """
-    tests if there is another vertex within rad of x,y. rad must be less than
+    tests if there is another vertex withlong* rad of x,y. rad must be less than
     the width of each zone.
     """
 
     #TODO: optimize this
 
-    cdef int i
-    cdef int j
+    cdef long i
+    cdef long j
     cdef sZ *zone
-    cdef int zi = self.__get_z(x,y,z)
+    cdef long zi = self.__get_z(x,y,z)
 
-    cdef int nz = self.nz
+    cdef long nz = self.nz
 
     cdef double dx
     cdef double dy
     cdef double dz
     cdef double rad2 = rad*rad
 
-    cdef int zz = int(z*nz)
-    cdef int zy = int(y*nz)
-    cdef int zx = int(x*nz)
+    cdef long zz = <long>(z*nz)
+    cdef long zy = <long>(y*nz)
+    cdef long zx = <long>(x*nz)
 
-    cdef int zvj
+    cdef long zvj
 
-    cdef int a
-    cdef int b
-    cdef int c
+    cdef long a
+    cdef long b
+    cdef long c
 
     for a in xrange(max(zx-1,0),min(zx+2,nz)):
       for b in xrange(max(zy-1,0),min(zy+2,nz)):
@@ -347,40 +347,40 @@ cdef class Zonemap3d:
   @cython.boundscheck(False)
   @cython.nonecheck(False)
   @cython.cdivision(True)
-  cdef int __sphere_vertices(
+  cdef long __sphere_vertices(
     self,
     double x,
     double y,
     double z,
     double rad,
-    int *vertices
+    long *vertices
   ) nogil:
 
-    cdef int i
-    cdef int j
+    cdef long i
+    cdef long j
     cdef sZ *zone
-    cdef int zi = self.__get_z(x,y,z)
+    cdef long zi = self.__get_z(x,y,z)
 
-    cdef int nz = self.nz
+    cdef long nz = self.nz
 
-    cdef int num = 0
+    cdef long num = 0
 
     cdef double dx
     cdef double dy
     cdef double dz
     cdef double rad2 = rad*rad
 
-    # cdef int zz = int(zi/nz/nz)
-    # cdef int zy = int((zi-nz*nz*zz)/nz)
-    # cdef int zx = int(zi-nz*nz*zz-nz*zy)
+    # cdef long zz = long(zi/nz/nz)
+    # cdef long zy = long((zi-nz*nz*zz)/nz)
+    # cdef long zx = long(zi-nz*nz*zz-nz*zy)
 
-    cdef int zz = int(z*nz)
-    cdef int zy = int(y*nz)
-    cdef int zx = int(x*nz)
+    cdef long zz = <long>(z*nz)
+    cdef long zy = <long>(y*nz)
+    cdef long zx = <long>(x*nz)
 
-    cdef int a
-    cdef int b
-    cdef int c
+    cdef long a
+    cdef long b
+    cdef long c
 
     for a in xrange(max(zx-1,0),min(zx+2,nz)):
       for b in xrange(max(zy-1,0),min(zy+2,nz)):
@@ -395,21 +395,18 @@ cdef class Zonemap3d:
             dz = z-self.Z[zone.ZV[j]]
 
             if dx*dx+dy*dy+dz*dz<rad2:
-              #print(a,b,c,j,zone.ZV[j])
               vertices[num] = zone.ZV[j]
               num += 1
-
-    #print('next')
 
     return num
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef list _perftest(self, int nmax, int num_points, int num_lookup):
+  cpdef list _perftest(self, long nmax, long num_points, long num_lookup):
 
     cdef np.ndarray[double, mode="c",ndim=2] a
-    cdef int i
+    cdef long i
     cdef double t1
     cdef double t2
     cdef list res = []
@@ -442,8 +439,8 @@ cdef class Zonemap3d:
 
     a = np.random.random((num_lookup,3))
     t1 = time()
-    cdef int asize = self.__get_max_sphere_count()
-    cdef int *vertices = <int *>malloc(asize*sizeof(int))
+    cdef long asize = self.__get_max_sphere_count()
+    cdef long *vertices = <long *>malloc(asize*sizeof(long))
     for i in xrange(num_lookup):
       self.__sphere_vertices(
         a[i,0],
@@ -472,49 +469,49 @@ cdef class Zonemap3d:
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef int add_vertex(self, int v1):
+  cpdef long add_vertex(self, long v1):
 
     return self.__add_vertex(v1)
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef int del_vertex(self, int v1):
+  cpdef long del_vertex(self, long v1):
 
     return self.__del_vertex(v1)
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cdef int __get_max_sphere_count(self) nogil:
+  cdef long __get_max_sphere_count(self) nogil:
 
     return self.greatest_zone_size*27
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef int update_v(self, int v1):
+  cpdef long update_v(self, long v1):
 
     return self.__update_v(v1)
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef int sphere_is_free(self, double x, double y, double z, double rad):
+  cpdef long sphere_is_free(self, double x, double y, double z, double rad):
 
     return self.__sphere_is_free(x, y, z, rad)
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef int get_max_sphere_count(self):
+  cpdef long get_max_sphere_count(self):
 
     return self.__get_max_sphere_count()
 
   @cython.wraparound(False)
   @cython.boundscheck(False)
   @cython.nonecheck(False)
-  cpdef int get_vnum(self):
+  cpdef long get_vnum(self):
 
     return self.vnum
 
@@ -525,7 +522,7 @@ cdef class Zonemap3d:
 
     cdef list res = []
     cdef dict d
-    cdef int i
+    cdef long i
 
     for i in xrange(self.total_zones):
 
